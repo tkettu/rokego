41ece30ed0d95f0d52ae7d4b5eba90ea70d15919
tkettu <terojkettunen@gmail.com>
2017-06-20 18:29:19 +0300

    Ready2 for heroku deployment.

--- distance_tracker/settings.py
+++ distance_tracker/settings.py
@@ -131,8 +131,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During devel
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # My settings
 LOGIN_URL = '/users/login/'
@@ -157,8 +163,8 @@ if os.getcwd() == '/app':
 	
 	# Static asset configuration
 	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-	#STATIC_ROOT = 'staticfiles'
-	STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+	STATIC_ROOT = 'staticfiles'
+	#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 	STATICFILES_DIRS = (
 		os.path.join(BASE_DIR, 'static'),
 	)
