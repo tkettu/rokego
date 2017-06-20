cb30a3ed75af163f7ac7c5d2f5425a210390b413
tkettu <terojkettunen@gmail.com>
2017-06-20 19:20:38 +0300

    Email settings to production.

--- distance_tracker/settings.py
+++ distance_tracker/settings.py
@@ -127,7 +127,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'rokegoteam@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
+EMAIL_PORT = 587
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
