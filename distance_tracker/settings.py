ad99baa45ee0a5bf21cd30dc9a6f5489f19c0a30
tkettu <terojkettunen@gmail.com>
2017-06-21 15:30:19 +0300

    Static-file settings modified.

--- distance_tracker/settings.py
+++ distance_tracker/settings.py
@@ -171,8 +171,8 @@ if os.getcwd() == '/app':
 	
 	# Static asset configuration
 	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-	STATIC_ROOT = 'staticfiles'
-	#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+	#STATIC_ROOT = 'staticfiles'
+	STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 	STATICFILES_DIRS = (
 		os.path.join(BASE_DIR, 'static'),
 	)
