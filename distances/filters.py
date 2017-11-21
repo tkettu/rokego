91803a365efada608706ec7e11e2fe76b322043e
tkettu <terojkettunen@gmail.com>
2017-11-21 16:05:40 +0200

    Cleaning and refactoring.

--- distances/filters.py
+++ distances/filters.py
@@ -1,76 +1,58 @@
 """ Filters for exercise forms """
-#Todo these should only apply for single users
+# Todo these should only apply for single users
 
-startdate = ''
-enddate = ''
+start_date = ''
+end_date = ''
 sport = 'all'
 
 import django_filters
 from .models import Exercise
 from .forms import DateInput
 
-from datetime import datetime
-
-#import distances.helpers.sports as spo
 import distances.json.sports as spo
-#from django_fil
 
 class ExerciseFilter(django_filters.FilterSet):
-	date = django_filters.DateFilter()
-	date__gt = django_filters.DateFilter(label = 'Start date', name='date',
-					lookup_expr='gte',
-					widget=DateInput())
-	date__lt = django_filters.DateFilter(label = 'End date', name='date', 
-					lookup_expr='lte',
-					widget=DateInput())
-	
-	startDate = date__gt
-	endDate = date__lt
-	#startDate = django_filters.DateFilter(
-	#			'start'
-	
-	sport = django_filters.MultipleChoiceFilter(choices=spo.get_sport_choices())
-	sub_sport = django_filters.CharFilter(max_length=25)
-	#sub_sport = django_filters.ChoiceFilter(choices=spo.getFieldChoices())
-	
-	class Meta:
-		model = Exercise
-		fields=('sport', 'sub_sport', 'date', 'startDate', 'endDate')
+    date = django_filters.DateFilter()
+    date__gt = django_filters.DateFilter(label='Start date', name='date',
+                                         lookup_expr='gte',
+                                         widget=DateInput())
+    date__lt = django_filters.DateFilter(label='End date', name='date',
+                                         lookup_expr='lte',
+                                         widget=DateInput())
+    startDate = date__gt
+    endDate = date__lt
+
+    sport = django_filters.MultipleChoiceFilter(choices=spo.get_sport_choices())
+    sub_sport = django_filters.CharFilter(max_length=25)
+
+    class Meta:
+        model = Exercise
+        fields = ('sport', 'sub_sport', 'date', 'startDate', 'endDate')
+
 
 class RecordFilter(django_filters.FilterSet):
-	sport = django_filters.MultipleChoiceFilter(choices=spo.get_sport_choices())
-	sub_sport = django_filters.CharFilter(max_length=25)
-	year = django_filters.NumberFilter(label='Year', name='date', lookup_expr='year')
-	#days = django_filters.NumberFilter(label='Day count', name='days')
-	
-	#def __init__(self, *args, **kwargs):
-		#super(RecordFilter, self).__init__(*args, **kwargs)
-		#if datetime.now().month > 1:
-			#self.form.initial['year'] = datetime.now().year
-		#else:
-			#self.form.initial['year'] = datetime.now().year - 1 
-		
-	
-	class Meta:
-		model = Exercise
-		fields=('sport', 'sub_sport', 'year')# 'days',)
+    sport = django_filters.MultipleChoiceFilter(choices=spo.get_sport_choices())
+    sub_sport = django_filters.CharFilter(max_length=25)
+    year = django_filters.NumberFilter(label='Year', name='date', lookup_expr='year')
+
+    class Meta:
+        model = Exercise
+        fields = ('sport', 'sub_sport', 'year')  # 'days',)
+
 
 class GraphFilter(django_filters.FilterSet):
-	date = django_filters.DateFilter()
-	date__gt = django_filters.DateFilter(label = 'Start date', name='date',
-					lookup_expr='gte',
-					widget=DateInput())
-	date__lt = django_filters.DateFilter(label = 'End date', name='date', 
-					lookup_expr='lte',
-					widget=DateInput())
-	
-	startDate = date__gt
-	endDate = date__lt
-	sport = django_filters.MultipleChoiceFilter(choices=spo.get_sport_choices())
-	
-	
-	class Meta:
-		model = Exercise
-		fields=('sport', 'startDate', 'endDate')
-	#def get_days(self):
-	#	return self.days
+    date = django_filters.DateFilter()
+    date__gt = django_filters.DateFilter(label='Start date', name='date',
+                                         lookup_expr='gte',
+                                         widget=DateInput())
+    date__lt = django_filters.DateFilter(label='End date', name='date',
+                                         lookup_expr='lte',
+                                         widget=DateInput())
+
+    startDate = date__gt
+    endDate = date__lt
+    sport = django_filters.MultipleChoiceFilter(choices=spo.get_sport_choices())
+
+    class Meta:
+        model = Exercise
+        fields = ('sport', 'startDate', 'endDate')
--- distances/graphs.py
+++ distances/graphs.py
@@ -3,6 +3,7 @@
 from .models import Exercise
 import numpy as np
 import matplotlib
+
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
@@ -11,236 +12,192 @@ import django
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 
-import matplotlib.cm as cm
 import matplotlib.patches as mpatches
 
 import distances.json.sports as spo
 
 from django.db.models import Min, Max
-#import PIL, PIL.Image, StringIO
-
-#def graphs(request, sport='Running'):
-	
-	#exercises = Exercise.objects.filter(owner=request.user, sport=sport)
-	
-	#fig = Figure()
-	#ax = fig.add_subplot(111)
-	#d = []
-	#t = []
-	#cc = []
-	#for e in exercises:
-		#d.append(e.distance)
-		#t.append(e.time_as_hours)
-		#cc.append(e.sport)
-	
-	##plt.scatter(d,t)
-	#ax.plot(d,t)
-	#canvas = FigureCanvas(fig)
-	#response = django.http.HttpResponse(content_type='image/png')
-	
-	#canvas.print_png(response)
-	#return response
-
 
 color_list = ['b', 'g', 'r', 'c', 'm', 'y', 'black', 'brown', 'purple', 'gold', 'darkgray', 'azure']
-#sl = spo.get_sport_choices()
-#sport_list = [s[0] for s in sl]
+
 dist_label = 'distance (km)'
 time_label = 'time (h)'
 
 
 def graphs2(exercises):
-	""" Scatter (time-distance) plot from exercises"""
-	""" TODO: if one sport --> Diff colors for sports type (sub sports)"""
-	
-	fig = Figure()
-	ax = fig.add_subplot(111)
-	d = []
-	t = []
-	cc = []
-	cc2 = []
-	for e in exercises:
-		d.append(e.distance)
-		t.append(e.time_as_hours)
-		cc.append(e.sport)
-		cc2.append(e.sub_sport)
-	
-	
-	recs = []
-	inds = list(set(cc))
-	
-	
-	nsports = len(inds)
-	
-	
-	color_dict = get_color_dict(inds)
-	#if len(inds) > 1:
-	#	sl = get_sport_list()
-	#else:
-	#	sl = get_sub_sport_list()
-	#	inds 
-	if nsports > 1:
-		sl = get_sport_list()
-		ccf = cc
-		
-	else:
-		s_type = inds[0]
-		ccf = make_ssport_list(cc2, s_type)
-		inds2 = list(set(ccf))
-		#inds2[0] = s_type
-		
-		sl = get_sub_sport_list(s_type)
-		
-		
-	try:
-		#ax.scatter(d,t, c=[ color_dict[i] for i in cc])
-		ax.scatter(d,t, c=[ color_dict[i] for i in ccf])
-	except KeyError:
-		# plot without colors
-		
-		ax.scatter(d,t)
-		
-	
-	
-	if nsports > 1:
-		for i in inds:#range(0,len(set(cc))):  # legend color for unique sport values
-			recs.append(mpatches.Rectangle((0,0),1,1,fc=color_list[sl.index(i)]))
-		fig.legend( recs, inds, 'right')
-	else:
-		for i in inds2:
-			recs.append(mpatches.Rectangle((0,0),1,1,fc=color_list[sl.index(i)]))
-		fig.legend( recs, inds2, 'right')
-	
-	#fig.legend( recs, sl, 'right')
-	#fig.suptitle("Scatter plot")
-	title = get_date_title(exercises)
-	fig.suptitle(title)
-	
-	ax.set_xlabel(dist_label)
-	ax.set_ylabel(time_label)
-	#ax.get_xaxis().get_major_formatter().set_scientific(False)
-	
-	ax.get_xaxis().get_major_formatter().set_useOffset(False)
-	ax.get_yaxis().get_major_formatter().set_useOffset(False)
-	#fig.axes(x='distance', y='time')
-	canvas = FigureCanvas(fig)
-	response = django.http.HttpResponse(content_type='image/png')
-	
-	canvas.print_png(response)
-	return response
+    """ Scatter (time-distance) plot from exercises"""
+
+    fig = Figure()
+    ax = fig.add_subplot(111)
+    distances = []
+    times = []
+    sports = []
+    sport_types = []
+    for e in exercises:
+        distances.append(e.distance)
+        times.append(e.time_as_hours)
+        sports.append(e.sport)
+        sport_types.append(e.sub_sport)
+
+    recs = []
+    sport_names = list(set(sports))
+
+    nsports = len(sport_names)
+
+    color_dict = get_color_dict(sport_names)
+
+    if nsports > 1:
+        sport_list = get_sport_list()
+        coloring_sports = sports
+
+    else:
+        s_type = sport_names[0]
+        coloring_sports = make_sub_sport_list(sport_types, s_type)
+        sub_sport_names = list(set(coloring_sports))
+
+        sport_list = get_sub_sport_list(s_type)
+
+    try:
+        # ax.scatter(d,t, c=[ color_dict[i] for i in cc])
+        ax.scatter(distances, times, c=[color_dict[i] for i in coloring_sports])
+    except KeyError:
+        # plot without colors
+        ax.scatter(distances, times)
+
+    if nsports > 1:
+        for i in sport_names:  # range(0,len(set(cc))):  # legend color for unique sport values
+            recs.append(mpatches.Rectangle((0, 0), 1, 1, fc=color_list[sport_list.index(i)]))
+        fig.legend(recs, sport_names, 'right')
+    else:
+        for i in sub_sport_names:
+            recs.append(mpatches.Rectangle((0, 0), 1, 1, fc=color_list[sport_list.index(i)]))
+        fig.legend(recs, sub_sport_names, 'right')
+
+    title = get_date_title(exercises)
+    fig.suptitle(title)
+
+    ax.set_xlabel(dist_label)
+    ax.set_ylabel(time_label)
+
+    ax.get_xaxis().get_major_formatter().set_useOffset(False)
+    ax.get_yaxis().get_major_formatter().set_useOffset(False)
+    # fig.axes(x='distance', y='time')
+    canvas = FigureCanvas(fig)
+    response = django.http.HttpResponse(content_type='image/png')
+
+    canvas.print_png(response)
+    return response
+
 
 def graph_dist_sum(exercises):
-	""" Cumulative distance of exercises """
-	fig = Figure()
-	ax = fig.add_subplot(111)
-	
-	exes = exercises.order_by('date')
-	
-	dist = np.array([e.distance for e in exes])
-	dat = [e.date for e in exes]
-	
-	cum_sum = np.cumsum(dist)
-	
-	ax.plot(dat,cum_sum)
-	ax.set_xlabel('Date')
-	ax.set_ylabel('Cumulative distance (km)')
-	fig.autofmt_xdate()
-	fig.suptitle(get_date_title(exercises))
-	canvas = FigureCanvas(fig)
-	response = django.http.HttpResponse(content_type='image/png')
-	
-	canvas.print_png(response)
-	return response
-	
+    """ Cumulative distance of exercises """
+    fig = Figure()
+    ax = fig.add_subplot(111)
+
+    exes = exercises.order_by('date')
+
+    dist = np.array([e.distance for e in exes])
+    date = [e.date for e in exes]
+
+    cum_sum = np.cumsum(dist)
+
+    ax.plot(date, cum_sum)
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Cumulative distance (km)')
+    fig.autofmt_xdate()
+    fig.suptitle(get_date_title(exercises))
+    canvas = FigureCanvas(fig)
+    response = django.http.HttpResponse(content_type='image/png')
+
+    canvas.print_png(response)
+    return response
+
+
 def box_plot(exercises, quant='distance'):
-	"""Returns boxplot (whiskers) from distances (or time) of exercises """
-	
-	# exercises to list of sport name and distances
-	el = list(exercises.values('sport', quant))
-	
-	#dd = []
-	#for e in exercises:
-	#	di = float(e.distance) # e.quant
-	#	dd.append(di)
-	
-	# el to list of distances [['Running_distances'],['Walking_distances']]
-	#sports = [
-	# ell = []
-	fig = Figure()
-	ax = fig.add_subplot(111)
-	
-	
-	#ax.boxplot(ell
-	dd = {}
-	for e in el:
-		di = float(e['distance'])
-		dd.setdefault(e['sport'], []).append(di)
-	
-	dl = []
-	dk = []
-	for k,v in dd.items():
-		dk.append(k)
-		dl.append(v)
-	
-	#ax.boxplot(dd)
-	ax.boxplot(dl)
-	
-	ax.set_xticklabels( dk)
-	ax.set_ylabel(dist_label)
-	fig.suptitle("Whisker {0}".format(get_date_title(exercises)))
-	
-	canvas = FigureCanvas(fig)
-	response = django.http.HttpResponse(content_type='image/png')
-	
-	canvas.print_png(response)
-	return response
-	
-	
+    """Returns boxplot (whiskers) from distances (or time) of exercises """
+
+    # exercises to list of sport name and distances
+    exercise_list = list(
+		exercises.values('sport', quant))
+
+    fig = Figure()
+    ax = fig.add_subplot(111)
+
+    # ax.boxplot(ell
+    distance_list = {}
+    for e in exercise_list:
+        #di = float(e['distance'])
+        distance_list.setdefault(e['sport'], []).append(float(e['distance']))
+
+    distances = []
+    names = []
+    for k, v in distance_list.items():
+        names.append(k)
+        distances.append(v)
+
+    # ax.boxplot(distances)
+    ax.boxplot(distances)
+
+    ax.set_xticklabels(names)
+    ax.set_ylabel(dist_label)
+    fig.suptitle("Whisker {0}".format(get_date_title(exercises)))
+
+    canvas = FigureCanvas(fig)
+    response = django.http.HttpResponse(content_type='image/png')
+
+    canvas.print_png(response)
+    return response
+
+
 def get_color_dict(sports):
-	if len(sports) > 1:
-		sport_list = get_sport_list()
-	else:
-		sport_list = get_sub_sport_list(sports[0])
-		
-	cd = {}
-	for i in range(len(sport_list)):
-		cd[sport_list[i]] = color_list[i]
-	
-	
-	return cd
+    if len(sports) > 1:
+        sport_list = get_sport_list()
+    else:
+        sport_list = get_sub_sport_list(sports[0])
+
+    color_dict = {}
+    for i in range(len(sport_list)):
+        color_dict[sport_list[i]] = color_list[i]
+
+    return color_dict
+
 
 def get_sport_list():
-	sl = spo.get_sport_choices()
-	sport_list = [s[0] for s in sl]	
-	return sport_list
+    sl = spo.get_sport_choices()
+    sport_list = [s[0] for s in sl]
+    return sport_list
+
 
 def get_sub_sport_list(s):
-	sl = spo.getFieldChoices(key_field=s)
-	
-	sport_list = [s[0] for s in sl]	
-	sport_list[0] = s
-	
-	return sport_list
+    sl = spo.getFieldChoices(key_field=s)
 
-def make_ssport_list(ll, s):
-	""" Fills empty and non-rights values in list ll with sport s"""
-	slist = get_sub_sport_list(s)
-	nlist = []
-	for i in ll:
-		if (i not in slist) | (i == ""):
-			nlist.append(s)
-		else:
-			nlist.append(i)
-	return nlist
-	
+    sport_list = [s[0] for s in sl]
+    sport_list[0] = s
+
+    return sport_list
+
+
+def make_sub_sport_list(sport_list, name):
+    """ Fills empty and non-rights values in list sport_list with sport name"""
+    slist = get_sub_sport_list(name)
+    nlist = []
+    for i in sport_list:
+        if (i not in slist) | (i == ""):
+            nlist.append(name)
+        else:
+            nlist.append(i)
+    return nlist
+
+
 def get_date_title(exercises):
-	""" Return min and max dates of exercises as string"""
-	""" Format mindate - maxdate """
-	mindate = exercises.aggregate(Min('date'))
-	mindates = mindate['date__min']
-	maxdate = exercises.aggregate(Max('date'))
-	maxdates = maxdate['date__max']
-	
-	retval = mindates.strftime("%d.%m.%y") + '-' + maxdates.strftime("%d.%m.%y")
-	return retval
-	#return str(mindates) + '-' + str(maxdates)
+    """ Return min and max dates of exercises as string"""
+    """ Format mindate - maxdate """
+    mindate = exercises.aggregate(Min('date'))
+    mindates = mindate['date__min']
+    maxdate = exercises.aggregate(Max('date'))
+    maxdates = maxdate['date__max']
+
+    retval = mindates.strftime("%d.%m.%y") + '-' + maxdates.strftime("%d.%m.%y")
+    return retval
+
+# return str(mindates) + '-' + str(maxdates)
